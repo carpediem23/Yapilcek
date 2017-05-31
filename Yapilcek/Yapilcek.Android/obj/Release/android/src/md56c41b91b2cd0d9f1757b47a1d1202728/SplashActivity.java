5d2a4489dc25b8bcd9b7414cebe6e024ee6b6c96
TURKIS-HOME\alptu <carpediem.2323@gmail.com>
2017-05-31 23:25:49 +0300

    First commit

---
+++ Yapilcek/Yapilcek.Android/obj/Release/android/src/md56c41b91b2cd0d9f1757b47a1d1202728/SplashActivity.java
@@ -0,0 +1,56 @@
+package md56c41b91b2cd0d9f1757b47a1d1202728;
+
+
+public class SplashActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"";
+		mono.android.Runtime.register ("Yapilcek.Droid.SplashActivity, Yapilcek.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("Yapilcek.Droid.SplashActivity, Yapilcek.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
